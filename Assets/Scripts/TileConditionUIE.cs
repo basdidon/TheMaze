f84b27398ef0e8a4ed3e492cdb700aa9b2f0f1ec
basdidon <39435008+basdidon@users.noreply.github.com>
2023-09-26 19:41:33 +0700

    -

--- Assets/Scripts/TileConditionUIE.cs
+++ Assets/Scripts/TileConditionUIE.cs
@@ -4,16 +4,23 @@ using UnityEditor.UIElements;
 using UnityEngine;
 using System;
 
+public enum TileRules { ANY = 0, CONNECT = 1, NOT_CONNECT = 2 }
+
+[Serializable]
+public struct TileRule4Dir
+{
+    public TileRules rule_n;
+    public TileRules rule_s;
+    public TileRules rule_w;
+    public TileRules rule_e;
+}
 
 [Serializable]
 public class TileCondition
 {
     public Sprite Sprite;
-    public bool isConnect_N;
-    public bool isConnect_S;
-    public bool isConnect_W;
-    public bool isConnect_E;
-    public string someText = "hehe";
+    public TileRule4Dir rule4Dir;
+    public int count;
 
     public bool TryGetSprite(int code, out Sprite sprite)
     {
@@ -31,55 +38,58 @@ public class TileCondition
     // Helper method to convert conditions to a binary code
     private int GetConditionCode()
     {
+        /*
         int code = 0;
         code |= (isConnect_N ? 1 : 0) << 3;
         code |= (isConnect_S ? 1 : 0) << 2;
         code |= (isConnect_W ? 1 : 0) << 1;
         code |= (isConnect_E ? 1 : 0);
-        return code;
+        return code;*/
+        return 0;
     }
 }
 
 [CustomPropertyDrawer(typeof(TileCondition))]
 public class TileConditionUIE : PropertyDrawer
 {
-    int count = 0;
+    Sprite crossSprite;
+    Sprite rightArrowSprite;
 
     public override VisualElement CreatePropertyGUI(SerializedProperty property)
     {
-        count = 0;
-
+        crossSprite = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Editor/UiDocs/Sprites/close.png");
+        rightArrowSprite = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Editor/UiDocs/Sprites/right-arrow.png");
         // Load Uxml
         var assetTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/UiDocs/CustomTileRuleEditor.uxml");
         var root = assetTree.Instantiate();
 
-        VisualElement objectFieldDisplay = root.Q<VisualElement>(null, new string[] { ObjectField.objectUssClassName });
-        VisualElement objectFieldDisplayIcon = objectFieldDisplay.Q<Image>();
-        //objectFieldDisplayIcon.style.display = DisplayStyle.None;
-        objectFieldDisplayIcon.RemoveFromClassList("unity-object-field-display__icon");
-        objectFieldDisplayIcon.RemoveFromClassList("unity-image");
-        VisualElement objectFieldInput = root.Q<VisualElement>(null, new string[] { ObjectField.inputUssClassName });
+        // Rule Picker
+        CreateRulePickers(root,property);
 
-        Label name = new()
-        {
-            text = count.ToString()
-        };
+        // Sprite Object Field
+        VisualElement objectFieldDisplay = root.Q<VisualElement>(null, new string[] { ObjectField.objectUssClassName });
+        Image objectFieldDisplayIcon = objectFieldDisplay.Q<Image>();
+        VisualElement objectFieldInput = root.Q<VisualElement>(null, new string[] { ObjectField.inputUssClassName });
         
         Image spritePreview = new ();
-        objectFieldInput.Insert(0, spritePreview);
-
+        objectFieldDisplay.Insert(1, spritePreview);
+        spritePreview.pickingMode = PickingMode.Ignore;
+        
         ObjectField spriteObjectField = root.Q<ObjectField>();
         spriteObjectField.RegisterValueChangedCallback((ev)=> {
             if (ev.newValue is Sprite sprite)
             {
+                objectFieldDisplayIcon.AddToClassList("hidden");
                 spritePreview.sprite = sprite;
-                count++;
-                name.text = count.ToString();
+            }
+            else if(ev.newValue == null)
+            {
+                spritePreview.sprite = null;
+                objectFieldDisplayIcon.RemoveFromClassList("hidden");
             }
         });
-        
-        root.Add(name);
 
+        // Binding data
         GameObject selectedObject = Selection.activeObject as GameObject;
         if (selectedObject != null)
         {
@@ -94,4 +104,80 @@ public class TileConditionUIE : PropertyDrawer
 
         return root;
     }
+
+    readonly string[] rulePickerKeywords = new string[] {"n","w",string.Empty,"e","s"};
+
+    void CreateRulePickers(VisualElement root,SerializedProperty property)
+    {
+        var topRow = root.Q<VisualElement>("rule-picker-top-row");
+        var middleRow = root.Q<VisualElement>("rule-picker-middle-row");
+        var bottomRow = root.Q<VisualElement>("rule-picker-bottom-row");
+
+        foreach(var keyword in rulePickerKeywords)
+        {
+            var rulePicker = new Image();
+            rulePicker.AddToClassList("condition-box");
+            if (keyword != string.Empty)
+            {
+                var propertyName = $"rule4Dir.rule_{keyword}";
+                rulePicker.sprite = GetRulePickerSprite(property.FindPropertyRelative(propertyName).enumValueIndex);
+                rulePicker.RegisterCallback<MouseDownEvent>(ev =>
+                {
+                    int enumValue = property.FindPropertyRelative(propertyName).enumValueIndex;
+                    enumValue = (enumValue + 1) % Enum.GetValues(typeof(TileRules)).Length;
+                    property.FindPropertyRelative(propertyName).enumValueIndex = enumValue;
+
+                    rulePicker.sprite = GetRulePickerSprite(enumValue);
+
+                    // Update Changed value
+                    property.serializedObject.ApplyModifiedProperties();
+                    property.serializedObject.Update();
+                });
+            }
+
+            // assign to thier row
+            switch (keyword)
+            {
+                case "n":
+                    topRow.Add(rulePicker);
+                    break;
+                case "w":case "":case "e":
+                    middleRow.Add(rulePicker);
+                    break;
+                case "s":
+                    bottomRow.Add(rulePicker);
+                    break;
+            }
+
+            if (keyword == string.Empty)
+                continue;
+
+            rulePicker.style.borderLeftWidth = 0;
+
+            if (keyword == "e")
+                continue;
+
+            rulePicker.style.rotate = keyword switch
+            {
+                "n" => new StyleRotate(new Rotate(new Angle(270f, AngleUnit.Degree))),
+                "w" => new StyleRotate(new Rotate(new Angle(180f, AngleUnit.Degree))),
+                "s" => new StyleRotate(new Rotate(new Angle(90f, AngleUnit.Degree))),
+                _ => throw new Exception("unexpected keyword"),
+            };
+            
+        }
+    }
+
+    Sprite GetRulePickerSprite(int tileRuleIndex)
+    {
+        return tileRuleIndex switch
+        {
+            (int)TileRules.ANY => null,
+            (int)TileRules.CONNECT => rightArrowSprite,
+            (int)TileRules.NOT_CONNECT => crossSprite,
+            _ => throw new Exception("unexpected enum index value."),
+        };
+    }
+
+    
 }
